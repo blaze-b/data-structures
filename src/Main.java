234ea8015fac96f3ca325efcb6f0bee6c8c0efb0
Brian <brian.blaze@lntinfotech.com>
2022-08-30 15:14:27 +0530

    final changes done in the data structures

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,33 @@
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         // Get the min element from the
         StackMinElement stackMinElement = new StackMinElement();
-        stackMinElement.push(12);
-        stackMinElement.push(12);
-        stackMinElement.push(12);
-        stackMinElement.push(11);
-        stackMinElement.push(20);
-        stackMinElement.push(10);
+        stackMinElement.push(4);
+        stackMinElement.push(5);
+        stackMinElement.push(3);
+        stackMinElement.push(2);
+        stackMinElement.push(0);
+        stackMinElement.push(1);
+        stackMinElement.push(7);
         System.out.println(stackMinElement.getMinElement());
-        System.out.println(stackMinElement.getSorted());
+        stackMinElement.pop();
+        System.out.println(stackMinElement.getMinElement());
+        stackMinElement.pop();
+        System.out.println(stackMinElement.getMinElement());
+        stackMinElement.pop();
+        System.out.println(stackMinElement.getMinElement());
+    }
+
+    public static void main(String[] args) throws Exception {
+        StackMinElement.StackMinElementUsingStack stackMinElementUsingStack =
+                new StackMinElement.StackMinElementUsingStack();
+        stackMinElementUsingStack.push(10);
+        stackMinElementUsingStack.push(11);
+        stackMinElementUsingStack.push(13);
+        System.out.println(stackMinElementUsingStack.getMin());
+        System.out.println(stackMinElementUsingStack.pop());
+        System.out.println(stackMinElementUsingStack.peek());
+        System.out.println(stackMinElementUsingStack);
     }
 }
--- src/StackMinElement.java
+++ src/StackMinElement.java
@@ -1,33 +1,114 @@
 import java.util.Stack;
 
+/**
+ * For the pushing 2*insertedElement - minElement
+ * For popping it is 2*minElement - poppedElement
+ */
+
 public class StackMinElement {
 
-    private Stack<Integer> elements;
+    private Stack<Integer> stack;
 
-    private Stack<Integer> minElement;
-
+    private int minElement;
 
     public StackMinElement() {
-        elements = new Stack<>();
-        minElement = new Stack<>();
+        stack = new Stack<>();
     }
 
-    public Integer push(int item) {
-        elements.push(item);
-        if(minElement.empty() || minElement.peek() > item)
-            minElement.push(item);
-        else
-            minElement.push(minElement.pop());
-        return item;
+    public Stack<Integer> getStack() {
+        return stack;
     }
 
-
-    public Integer getMinElement() {
-        return minElement.peek();
+    public void setStack(Stack<Integer> stack) {
+        this.stack = stack;
     }
 
-    public Stack<Integer> getSorted() {
+    public int getMinElement() {
         return minElement;
     }
 
+    public void peek() {
+        if (stack.isEmpty()) {
+            System.out.println("Stack is empty ");
+            return;
+        }
+        Integer t = stack.peek();
+        System.out.print("Top Most Element is: ");
+        if (t < minElement)
+            System.out.println(minElement);
+        else
+            System.out.println(t);
+    }
+
+    public void pop() {
+        if (stack.isEmpty()) {
+            System.out.println();
+            return;
+        }
+        System.out.println("Top element on the stack: " + stack);
+        Integer t = stack.pop();
+        if (t < minElement) {
+            minElement = 2 * minElement - t;
+            System.out.println(minElement);
+        } else
+            System.out.println(t);
+    }
+
+    public void push(int val) {
+        if (stack.isEmpty()) {
+            minElement = val;
+            stack.push(val);
+            System.out.println("Inserted value : " + val);
+            return;
+        }
+        if (val < minElement) {
+            stack.push(2 * val - minElement);
+            minElement = val;
+        } else
+            stack.push(val);
+    }
+
+    public static class StackMinElementUsingStack {
+
+        private Stack<Integer> elements;
+
+        private Stack<Integer> minElementStack;
+
+        public StackMinElementUsingStack() {
+            elements = new Stack<>();
+            minElementStack = new Stack<>();
+        }
+
+        public void push(int number) {
+            elements.push(number);
+            if (minElementStack.isEmpty() || number < minElementStack.peek()) {
+                minElementStack.push(number);
+            } else
+                minElementStack.push(minElementStack.pop());
+            System.out.println("Min element::" + minElementStack);
+        }
+
+        public Integer pop() throws Exception {
+            if(minElementStack.isEmpty() && elements.isEmpty())
+                throw new Exception("No element...");
+            final Integer pop = this.elements.pop();
+            final Integer minElement = this.getMin();
+
+            System.out.println("Final elements::" + elements);
+            System.out.println("Min element list::" + minElementStack);
+            return pop;
+        }
+
+        public Integer peek() throws Exception {
+            if(minElementStack.isEmpty())
+                throw new Exception("No element...");
+            return elements.elementAt(minElementStack.peek());
+        }
+
+        public Integer getMin() throws Exception {
+            if(minElementStack.isEmpty())
+                throw new Exception("No element...");
+            return minElementStack.peek();
+        }
+    }
 }
